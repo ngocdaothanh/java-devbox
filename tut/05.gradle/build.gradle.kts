2fbee190ac84bdb182dc90aff2e3cf02e053dfc0
duy1723 <vuquangduypct@gmail.com>
2025-12-08 22:38:24 +1100

    Add gradlew

---
+++ tut/05.gradle/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("java")
+    id("application")
+}
+
+application {
+    mainClass = "org.example.Main"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
