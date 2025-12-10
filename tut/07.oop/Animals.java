56bccb4ed122cdbddff67b77e5a66f873a8faed0
Ngoc Dao <ngocdaothanh@gmail.com>
2025-12-10 22:36:43 +1100

    Add OOP example

---
+++ tut/07.oop/Animals.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Abstraction (contract):
+// Inside an interface, we only have methods.
+interface Animal {
+    void talk();
+}
+
+// Encapsulation.
+//
+// Abstraction:
+// This class is an Animal, but it doesn't implement "talk" method yet.
+// So it needs to be an abstract class.
+abstract class NamedAnimal implements Animal {
+    // Properties
+    private final String name;
+
+    public NamedAnimal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+// Inheritance:
+// Code reuse
+class Cat extends NamedAnimal {
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public void talk() {
+        System.out.println("Meow, my name is " + getName());
+    }
+}
+
+// Inheritance:
+// Code reuse
+class Dog extends NamedAnimal {
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public void talk() {
+        System.out.println("Woof, my name is " + getName());
+    }
+}
+
+public class Animals {
+    public static void main(String[] args) {
+        // We can only create "new" instances from concrete classes
+        Animal a1 = new Cat("Kitty");
+        Animal a2 = new Dog("Pluto");
+
+        List<Animal> animals = new ArrayList<>();
+        animals.add(a1);
+        animals.add(a2);
+
+        printAnimals(animals);
+    }
+
+    private static void printAnimals(List<Animal> animals) {
+        // Polymorphism:
+        // Loop through the items in the collection of Animal
+        // and call "talk".
+        for (Animal a : animals) {
+            // Depending on the concrete class of the item,
+            // the behaviour will be different.
+            a.talk();
+        }
+    }
+}
---
+++ tut/07.oop/README.md
@@ -0,0 +1,27 @@
+# 4 principles of OOP
+
+## Abstraction
+
+Abstraction is the most important principle.
+
+"Program to interfaces, not to concrete classes."
+
+The purpose is to create the structure/skeleton of the program.
+
+2 ideas:
+* Interfaces
+* Abstract classes
+
+## Encapsulation
+
+* For bundling variables and functions together
+* Data hiding: we should always access the private data via public methods
+
+## Inheritance
+
+For code reuse.
+
+## Polymorphism
+
+We have a collection of items,
+and the behaviour of each item depends on the concrete class of the item.
