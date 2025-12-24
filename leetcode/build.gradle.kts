2857f5126f2be2ea4878c86b812549399d8325b2
Ngoc Dao <ngocdaothanh@gmail.com>
2025-12-24 23:29:11 +1100

    Update JUnit from 5 to 6 (latest)

--- leetcode/build.gradle.kts
+++ leetcode/build.gradle.kts
@@ -10,8 +10,12 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:6.0.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.test {
